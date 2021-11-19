af494e7ffcd187aec689f9c214d6844cd0f3f579
John Michael Delgado <johnmdelgado@Johns-MBP.attlocal.net>
2021-11-19 16:49:52 -0700

    added password encoding so user passwords would not be stored in the database as clear text

--- src/main/java/com/cs4374/finalproject/configuration/securityConfiguration.java
+++ src/main/java/com/cs4374/finalproject/configuration/securityConfiguration.java
@@ -1,9 +1,12 @@
 package com.cs4374.finalproject.configuration;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
@@ -19,5 +22,12 @@ public class securityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    // This is where we are going to be decrypting the passwords using spring
+    // best practice and the strongest password algorithm.
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/cs4374/finalproject/services/AuthService.java
+++ src/main/java/com/cs4374/finalproject/services/AuthService.java
@@ -4,6 +4,7 @@ import com.cs4374.finalproject.dataTransferObject.RegisterRequest;
 import com.cs4374.finalproject.repositories.UserRepository;
 import com.cs4374.finalproject.siteElements.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,21 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getPassword());
         userRepository.save(user);
+    }
+
+    // Here we don't want clear text passwords in the database for security reasons
+    // what we'll do is encrypt the password that the user provided
+
+    private String encodePassword(String password){
+        return passwordEncoder.encode(password);
     }
 }
